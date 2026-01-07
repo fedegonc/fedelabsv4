101fa60cf93954441526c5208442820ccdfaa5ed
fedegonc <goncalvezfede@gmail.com>
2026-01-07 17:46:10 -0300

    Add missing GetMapping import to AboutController

--- src/main/java/com/fedelabsv4/controller/AboutController.java
+++ src/main/java/com/fedelabsv4/controller/AboutController.java
@@ -2,6 +2,7 @@ package com.fedelabsv4.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutController {
