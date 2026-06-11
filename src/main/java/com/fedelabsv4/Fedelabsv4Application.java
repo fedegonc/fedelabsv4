e91722e276902d118f1c6ba5cfc24ef311b8ac8c
fedegonc <goncalvezfede@gmail.com>
2026-06-10 22:28:01 -0300

    sync data

--- src/main/java/com/fedelabsv4/Fedelabsv4Application.java
+++ src/main/java/com/fedelabsv4/Fedelabsv4Application.java
@@ -2,10 +2,19 @@ package com.fedelabsv4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Fedelabsv4Application {
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty(
+            "spring.data.mongodb.uri",
+            dotenv.get("SPRING_DATA_MONGODB_URI")
+        );
+
         SpringApplication.run(Fedelabsv4Application.class, args);
     }
 }
